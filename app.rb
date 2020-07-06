9a15106eebd8fec34cc80713bc2e8ea8f90dc795
serj <seregaproger@yandex.ru>
2020-07-06 21:37:09 +0300

    add db users

--- app.rb
+++ app.rb
@@ -1,6 +1,21 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'	
+
+configure do
+	$db = get_db
+	$db.execute 'CREATE TABLE IF NOT EXISTS
+		"Users" 
+		(
+			"id" 		INTEGER PRIMARY KEY AUTOINCREMENT, 
+			"username" 	TEXT, 
+			"phone" 	TEXT, 
+			"datestamp" TEXT, 
+			"barber" 	TEXT, 
+			"color" 	TEXT
+			)'
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
@@ -14,23 +29,41 @@ get '/visit' do
 	erb :visit
 end
 
+
+
 post '/visit' do
 
-	@username = params[:username]
-	@phone = params[:phone]
-	@datetime = params[:datetime]
-	@barber = params[:barber]
-	@color = params[:color]
+		@username = params[:username]
+		@phone = params[:phone]
+		@datetime = params[:datetime]
+		@barber = params[:barber]
+		@color = params[:color]
 
-hh = {:username => 'Введите имя',
-		:phone => 'Введите телефон',
-		:datetime => 'Введите дату'}
+		hh = {:username => 'Введите имя',
+			:phone => 'Введите телефон',
+			:datetime => 'Введите дату'}
 
-@error = hh.select {|key, | params[key] == ""}.values.join(", ")
+		@error = hh.select {|key, | params[key] == ""}.values.join(", ")
 
-if @error != ''
-	return erb :visit
-end
+		if @error != ''
+			return erb :visit
+		end
 
-	erb "OK. #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
+#		db = get_db
+		$db.execute 'insert into
+			Users
+				(
+					username,
+					phone,
+					datestamp,
+					barber,
+					color
+				)
+				values(	?, ?, ?, ?, ?)', [@username, @phone, @datetime, @barber, @color]
+
+		erb "OK. #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
+	end
+
+def get_db
+	return SQLite3::Database.new 'shop.db'
 end
