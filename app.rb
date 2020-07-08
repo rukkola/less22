872b9aa6fc64cc5b3caf230fced5d29d2dfa4670
serj <seregaproger@yandex.ru>
2020-07-08 18:01:09 +0300

    add blog

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ def seed_db db, barbers
 	end	
 end
 
-before do #используется до всех функций
+before do #используется до всех get/post  при перезагрузке любой страницы
 	@barbers = $db.execute 'select * from Barbers'
 end
 
@@ -42,10 +42,30 @@ configure do
 
 	seed_db $db, ['Петя', 'Люда', 'Вася', 'Толик']
 
+	$db.execute 'CREATE TABLE IF NOT EXISTS
+		Posts 
+		(
+			id INTEGER PRIMARY KEY AUTOINCREMENT, 
+			created_date DATE, 
+			content TEXT
+		)'
+
+	$db.execute 'CREATE TABLE IF NOT EXISTS
+		Comments
+		(
+			id INTEGER PRIMARY KEY AUTOINCREMENT, 
+			created_date DATE, 
+			content TEXT,
+			post_id INTEGER
+		)'	
 end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+
+	@results_post = $db.execute 'select * from Posts order by id desc'
+
+	erb :index
 end
 
 get '/about' do
@@ -101,4 +121,41 @@ def get_db
 	db = SQLite3::Database.new 'shop.db'
 	db.results_as_hash = true
 	return db 
+end
+
+get '/new' do
+  	erb :new
+end
+
+post '/new' do
+  content = params[:content]
+
+  if content.length == 0
+  	@error = 'Введите текст'
+  	return erb :new
+  end
+
+  $db.execute'insert into Posts (content, created_date) values ( ?, datetime())', [content]
+
+  redirect to '/'
+#  erb "You typed #{content}"
+end
+
+get '/details/:post_id' do
+	post_id = params[:post_id]
+
+	results_post = $db.execute 'select * from Posts where id = ?', [post_id]
+	@row = results_post[0]
+
+	@comments = $db.execute 'select * from Comments where post_id = ? order by id', [post_id]
+
+	erb :details
+end
+
+post '/details/:post_id' do
+	post_id = params[:post_id]
+	content = params[:content]	
+	$db.execute 'insert into Comments (content, created_date, post_id) values ( ?, datetime(), ?)', [content, post_id]
+
+	redirect to('/details/' + post_id)
 end
